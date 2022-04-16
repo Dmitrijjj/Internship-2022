54a312adf3664e5ebe6a5d7743815aa8a6a6d9a5
Alina Potupchik <linusechkapotupchik@gmail.com>
2022-04-16 21:39:18 +0300

    initial commit

--- docker/scripts/run.sh
+++ docker/scripts/run.sh
@@ -1,9 +1,9 @@
 docker network create my-golden-gate 2> /dev/null
 
-docker-compose -p symfony-test --env-file ./.env up -d --build --force-recreate --remove-orphans || exit
+docker-compose --env-file ./.env up -d --build --force-recreate --remove-orphans || exit
 
-docker-compose -p symfony-test exec mysql bash -c "mysql -uroot -proot -e \"IF NOT EXISTS(SELECT * FROM sys.databases WHERE name = 'master') BEGIN create database master CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;\" END"
+docker-compose exec mysql bash -c "mysql -uroot -proot -e \"IF NOT EXISTS(SELECT * FROM sys.databases WHERE name = 'master') create database master CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;\" "
 
-docker-compose -p symfony-test exec php-fpm bash -c "cd /var/www && COMPOSER_MEMORY_LIMIT=-1 composer install" || exit
+docker-compose exec php-fpm bash -c "cd /var/www && COMPOSER_MEMORY_LIMIT=-1 composer install" || exit
 
-docker-compose -p symfony-test exec php-fpm bash -c "php artisan migrate" || exit
+docker-compose exec php-fpm bash -c "php artisan migrate" || exit
